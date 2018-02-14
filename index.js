a0206328c8541a7d01d950a6ab2fc6311580f2e1
Alessandro Rabitti <axel83.beta@gmail.com>
2018-02-15 00:53:12 +0100

    installed eslint modules for IDE

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
-const Weather = require('./lib/Weather');
+/*eslint-env node*/
+
+const Weather = require("./lib/Weather");
 const weather = new Weather();
 
 weather.getCurrentWeatherByCity("Reggio nell'Emilia");
 
 weather.getCurrentWeatherByCoordinates(52.379189, 4.899431);
 
-console.log('I am checking weather forecasts');
+console.log("I am checking weather forecasts");
