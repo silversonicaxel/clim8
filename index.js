d65235cc8ba1547c0be28ce88ba7097127494fe0
Alessandro Rabitti <a.rabitti@tmg.nl>
2018-02-16 09:57:59 +0100

    Improved commander index.js

--- index.js
+++ index.js
@@ -5,18 +5,34 @@ const clim8Program = require("commander");
 const Weather = require("./lib/Weather");
 const weather = new Weather();
 
-// cli program
-clim8Program
-    .version(clim8Pkg.version)
-    .option("-C, --city <city>", "Provide city")
-    .option("-L, --coordinates <latitude,longitude>", "Provide latitude and longitude", items => items.split(","))
-    .parse(process.argv);
+const EXIT_ERROR = 1;
 
 
-if (clim8Program.city) {
-    weather.getCurrentWeatherByCity(clim8Program.city);
-}
+(async () => {
+    // clim8 program
+    clim8Program
+        .version(clim8Pkg.version)
+        .usage("[options] <option>")
+        .option("-C, --city <city>", "Provide city")
+        .option("-L, --coordinates <latitude,longitude>", "Provide latitude and longitude", items => items.split(","))
+        .parse(process.argv);
 
-if (clim8Program.coordinates && clim8Program.coordinates.length === 2) {
-    weather.getCurrentWeatherByCoordinates(clim8Program.coordinates[0], clim8Program.coordinates[1]);
-}
+
+    try {
+        if (clim8Program.city) {
+            weather.getCurrentWeatherByCity(clim8Program.city);
+        }
+
+        if (clim8Program.coordinates) {
+            if (clim8Program.coordinates.length !== 2) {
+                throw new Error("Coordinates must be 2, latitude and longitude");
+            }
+
+            weather.getCurrentWeatherByCoordinates(clim8Program.coordinates[0], clim8Program.coordinates[1]);
+        }
+    }
+    catch (error) {
+        console.log(error.message);
+        process.exit(EXIT_ERROR);
+    }
+})();
