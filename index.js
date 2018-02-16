e93f706f5beb441e822ead1e5e25afcb6c15a490
Alessandro Rabitti <axel83.beta@gmail.com>
2018-02-16 01:01:12 +0100

    setup basic cli options

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 #!/usr/bin/env node
+const clim8Pkg = require("./package.json");
+const clim8Program = require("commander");
+
 const Weather = require("./lib/Weather");
 const weather = new Weather();
 
-weather.getCurrentWeatherByCity("Reggio nell'Emilia");
+// cli program
+clim8Program
+    .version(clim8Pkg.version)
+    .option("-C, --city <city>", "Provide city")
+    .option("-L, --coordinates <latitude,longitude>", "Provide latitude and longitude", items => items.split(","))
+    .parse(process.argv);
 
-weather.getCurrentWeatherByCoordinates(52.379189, 4.899431);
 
-console.log("I am checking weather forecasts");
+if (clim8Program.city) {
+    weather.getCurrentWeatherByCity(clim8Program.city);
+}
+
+if (clim8Program.coordinates && clim8Program.coordinates.length === 2) {
+    weather.getCurrentWeatherByCoordinates(clim8Program.coordinates[0], clim8Program.coordinates[1]);
+}
